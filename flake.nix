e7b90624005d8681bd63aab6183103c859a2d363
Stefan Junker <mail@stefanjunker.de>
2025-09-11 17:08:30 +0200

    init from custom blueprint template

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "blueprint-universe";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    crane.url = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.blueprint {
+      inherit inputs;
+      prefix = "nix/";
+
+      # change this if unfree software is required.
+      nixpkgs.config.allowUnfree = false;
+    };
+}
---
+++ nix/formatter.nix
@@ -0,0 +1,83 @@
+{
+  pkgs,
+  inputs,
+  perSystem,
+  ...
+}:
+let
+  settingsNix = {
+    package = perSystem.nixpkgs.treefmt;
+
+    projectRootFile = ".git/config";
+
+    programs = {
+      nixfmt.enable = true;
+      deadnix = {
+        enable = true;
+        no-underscore = true;
+      };
+      statix.enable = true;
+
+      rustfmt = {
+        enable = true;
+        edition = "2024";
+      };
+
+      gofmt.enable = true;
+
+      shfmt.enable = true;
+
+      taplo.enable = true;
+
+      prettier.enable = true;
+
+      formatjson5.enable = true;
+
+      just.enable = true;
+    }
+    // pkgs.lib.optionalAttrs (pkgs.system != "riscv64-linux") { shellcheck.enable = true; };
+
+    settings = {
+      global.excludes = [
+        "LICENSE"
+        # unsupported extensions
+        "*.{gif,png,svg,tape,mts,lock,mod,sum,env,envrc,gitignore}"
+
+        # link to a read-only store-path
+        "treefmt.toml"
+      ];
+
+      formatter = {
+        deadnix = {
+          priority = 1;
+        };
+
+        nixfmt = {
+          priority = 2;
+        };
+
+        statix = {
+          priority = 3;
+        };
+
+        prettier = {
+          options = [
+            "--tab-width"
+            "2"
+          ];
+          includes = [ "*.{css,html,js,json,jsx,md,mdx,scss,ts,yaml}" ];
+        };
+      };
+    };
+  };
+
+  treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs settingsNix;
+
+in
+treefmtEval.config.build.wrapper.overrideAttrs (_: {
+  passthru = {
+    inherit (treefmtEval.config) package settings;
+    inherit (treefmtEval) config;
+    inherit settingsNix;
+  };
+})
