9afe7d011ac7cf21b5bdc0bee0e9f5696e432698
Stefan Junker <mail@stefanjunker.de>
2025-11-28 16:21:13 +0100

    fixup! feat: init nixos-installer iso

--- nix/packages/nixos-installer.nix
+++ nix/packages/nixos-installer.nix
@@ -5,69 +5,63 @@
   ...
 }:
 
-let
-  drv =
-    (inputs.nixpkgs.lib.nixosSystem {
-      inherit (pkgs.stdenv.targetPlatform) system;
-      modules = [
-        (
-          { modulesPath, ... }:
-          {
-            imports = [
-              inputs.srvos.nixosModules.mixins-nix-experimental
-              {
-                nix.settings.experimental-features = [
-                  "nix-command"
-                  "flakes"
-                ];
-              }
+# TODO: is there a better way in blueprint to filter this out?
+if !pkgs.stdenv.isLinux then
+  pkgs.emptyFile
+else
+  (inputs.nixpkgs.lib.nixosSystem {
+    inherit (pkgs.stdenv.targetPlatform) system;
+    modules = [
+      (
+        { modulesPath, ... }:
+        {
+          imports = [
+            inputs.srvos.nixosModules.mixins-nix-experimental
+            {
+              nix.settings.experimental-features = [
+                "nix-command"
+                "flakes"
+              ];
+            }
 
-              (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
-              (modulesPath + "/profiles/minimal.nix")
+            (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
+            (modulesPath + "/profiles/minimal.nix")
 
-              ./nixos-installer/configuration.nix
+            ./nixos-installer/configuration.nix
 
-              {
-                # Remove perl from activation
-                boot.initrd.systemd.enable = true;
-                system.etc.overlay.enable = true;
-                services.userborn.enable = true;
+            {
+              # Remove perl from activation
+              boot.initrd.systemd.enable = true;
+              system.etc.overlay.enable = true;
+              services.userborn.enable = true;
 
-                # Random perl remnants
-                system.tools.nixos-generate-config.enable = false;
-                boot.loader.grub.enable = false;
-                environment.defaultPackages = [ ];
-                documentation.info.enable = false;
-                documentation.nixos.enable = false;
-              }
-            ];
+              # Random perl remnants
+              system.tools.nixos-generate-config.enable = false;
+              boot.loader.grub.enable = false;
+              environment.defaultPackages = [ ];
+              documentation.info.enable = false;
+              documentation.nixos.enable = false;
+            }
+          ];
 
-            nix = {
-              enable = true;
-              settings = {
+          nix = {
+            enable = true;
+            settings = {
 
-              };
             };
+          };
 
-            # TODO: some day this won't be needed for an install
-            system.disableInstallerTools = false;
-            programs.nano.enable = false;
-            programs.fuse.enable = false;
-            security.sudo.enable = true;
-            networking.firewall.enable = false;
+          # TODO: some day this won't be needed for an install
+          system.disableInstallerTools = false;
+          programs.nano.enable = false;
+          programs.fuse.enable = false;
+          security.sudo.enable = true;
+          networking.firewall.enable = false;
 
-            environment.systemPackages = [
-              perSystem.self.rust-workspace
-            ];
-          }
-        )
-      ];
-    }).config.system.build.isoImage.overrideDerivation
-      (attrs: {
-        meta = attrs.meta // {
-          platforms = pkgs.lib.platforms.linux;
-        };
-      });
-
-in
-if pkgs.stdenv.isLinux then drv else pkgs.emptyFile
+          environment.systemPackages = [
+            perSystem.self.rust-workspace
+          ];
+        }
+      )
+    ];
+  }).config.system.build.isoImage
